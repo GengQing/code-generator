70769d17d8e84ebb093c05859e164893c420e2b9
GengQing <716435803@qq.com>
2018-06-06 18:34:02 +0800

    init project

---
+++ src/main/java/pers/gengq/code/generator/CodeGeneratorApplication.java
@@ -0,0 +1,10 @@
+package pers.gengq.code.generator;
+
+/**
+ * Created by gengqing on 6/6/2018
+ **/
+public class CodeGeneratorApplication {
+    public static void main(String[] args) {
+        System.out.println("This application is used for generate java code from swagger file");
+    }
+}
