a56e8c0e455a9049671c7a02f5bdb411170b2484
Qing Geng <qing.geng@ericsson.com>
2018-06-07 16:30:08 +0800

    parse swagger test

---
+++ src/test/java/pers/gengq/code/generator/SwaggerTest.java
@@ -0,0 +1,40 @@
+package pers.gengq.code.generator;
+
+import io.swagger.models.Model;
+import io.swagger.models.Swagger;
+import io.swagger.models.properties.Property;
+import io.swagger.parser.SwaggerParser;
+import io.swagger.parser.util.SwaggerDeserializationResult;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+
+
+/**
+ * Created by gengqing on 6/7/2018
+ **/
+public class SwaggerTest {
+
+    private String swagger = "swagger.yaml";
+
+    @Test
+    public void parseSwaggerTest() {
+        SwaggerDeserializationResult result =
+                new SwaggerParser().readWithInfo(swagger, Collections.emptyList(), true);
+        result.getMessages().forEach(System.out::println);
+        Swagger swagger = result.getSwagger();
+        assertNotNull(swagger);
+        Map<String, Model> defs = swagger.getDefinitions();
+        defs.forEach(this::println);
+    }
+
+    public void println(String s, Model model) {
+        System.out.println(s + " property--------------");
+        Map<String, Property> propertyMap = model.getProperties();
+        propertyMap.forEach((s1, property) -> System.out.println(s1 + " : " + property.getType()));
+    }
+
+}
